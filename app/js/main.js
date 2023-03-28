6df1aaff481046a74dd2fab7f23305c0ad525bcf
Lada-Novak <lada4445@yandex.ru>
2023-03-28 18:31:43 +0200

    added

---
+++ app/js/main.js
@@ -0,0 +1,152 @@
+$(function () {
+  // addaptiv
+  $('.menu__btn').on('click', function () {
+    $('.menu__list').toggleClass('menu__list--active');
+  });
+
+  $('.footer-top__title').on('click', function(){
+    $(this).siblings().slideToggle();
+    $(this).toggleClass('active');
+  });
+
+  //btn shop-filter
+  $('.shop__filter-btn').on('click', function(){
+    $('.shop__filters').slideToggle();
+  });
+
+  // product - tabs
+  $('.product-tabs__top-item').on('click', function(e){
+    e.preventDefault();
+    $('.product-tabs__top-item').removeClass('product-tabs__top-item--active');
+    $(this).addClass('product-tabs__top-item--active');
+
+    $('.product-tabs__content-item').removeClass('product-tabs__content-item--active');
+    $($(this).attr('href')).addClass('product-tabs__content-item--active');
+  });
+
+  $('.shop-content__filter-btn').on('click', function () {
+    $('.shop-content__filter-btn').removeClass('shop-content__filter-btn--active');
+    $(this).addClass('shop-content__filter-btn--active');
+  });
+
+  $('.button-list').on('click', function(){
+    $('.product-item').addClass('product-item--list');
+    $('.shop-content__inner').addClass('shop-content__nogrid');
+  });
+  $('.button-grid').on('click', function () {
+    $('.product-item').removeClass('product-item--list');
+    $('.shop-content__inner').removeClass('shop-content__nogrid');
+  });
+  // styler
+  $('.select-style, .product-one__item-num').styler();
+
+  // ion - rangeslider
+  $('.filter-price__input').ionRangeSlider({
+    type: "double",
+    prefix:"$",
+    onStart: function (data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
+    onChange: function (data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
+  });
+
+  // slick
+  $('.blog-page__slider').slick({
+    prevArrow: '<button type = "button" class = "slick-prev"><svg xmlns="http://www.w3.org/2000/svg"  width="9px" height="14px" viewBox="0 0 9 18" version="1.1"><g id="surface1"><path d="M 6.75 15.75 C 6.460938 15.75 6.175781 15.640625 5.953125 15.421875 L 0.328125 9.796875 C -0.109375 9.355469 -0.109375 8.644531 0.328125 8.203125 L 5.953125 2.578125 C 6.394531 2.140625 7.105469 2.140625 7.546875 2.578125 C 7.984375 3.019531 7.984375 3.730469 7.546875 4.171875 L 2.714844 9 L 7.546875 13.832031 C 7.984375 14.269531 7.984375 14.980469 7.546875 15.421875 C 7.328125 15.640625 7.039062 15.75 6.75 15.75 Z M 6.75 15.75 "/></g></svg></button>',
+    nextArrow: '<button type = "button" class = "slick-next"><svg width="9px" height="14px" viewBox="0 0 9 18" version="1.1"><g id="surface1"><path d="M 2.25 15.75 C 1.960938 15.75 1.675781 15.640625 1.453125 15.421875 C 1.015625 14.980469 1.015625 14.269531 1.453125 13.828125 L 6.285156 9 L 1.453125 4.167969 C 1.015625 3.730469 1.015625 3.019531 1.453125 2.578125 C 1.894531 2.140625 2.605469 2.140625 3.046875 2.578125 L 8.671875 8.203125 C 9.109375 8.644531 9.109375 9.355469 8.671875 9.792969 L 3.046875 15.417969 C 2.824219 15.640625 2.539062 15.75 2.25 15.75 Z M 2.25 15.75 "/></g></svg></button>',
+    infinite:false,
+  });
+
+  $('.top-slider__inner').slick({
+    dots: true,
+    arrows: false,
+    fade: true,
+    autoplay: true,
+    autoplaySpead: 2000,
+  });
+
+  $('.product-slide__thumb').slick({
+    asNavFor: '.product-slide__big',
+    focusOnSelect:true,
+    slidesToShow:4,
+    slidesToScroll:1,
+    vertical:true,
+    draggable:false
+  });
+  $('.product-slide__big').slick({
+    asNavFor: '.product-slide__thumb',
+    draggable: false,
+    arrows: false,
+    fade:true,
+    responsive:[
+      {
+        breakpoint:1051,
+        settings:{
+          draggable: true,
+        }
+      },
+    ]
+  });
+
+  // rateYo
+  $(".star").rateYo({
+    starWidth: "17px",
+    normalFill:"#ccccce",
+    ratedFill:"#ffc35b",
+    readOnly: true,
+    starSvg: '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="18px" height="16px" viewBox="0 0 18 16" version="1.1"><g id = "surface1" ><path style = " stroke:none;fill-rule:nonzero;fill-opacity:1;"d = "M 11.914062 4.695312 L 16.402344 5.359375 C 16.773438 5.414062 17.085938 5.675781 17.207031 6.035156 C 17.324219 6.398438 17.226562 6.789062 16.960938 7.058594 L 13.703125 10.253906 L 14.472656 14.835938 C 14.535156 15.210938 14.382812 15.589844 14.070312 15.8125 C 13.757812 16.035156 13.351562 16.0625 13.015625 15.882812 L 9.003906 13.742188 L 4.992188 15.882812 C 4.65625 16.0625 4.246094 16.035156 3.9375 15.8125 C 3.628906 15.589844 3.472656 15.210938 3.539062 14.835938 L 4.304688 10.253906 L 1.050781 7.058594 C 0.78125 6.789062 0.683594 6.398438 0.804688 6.035156 C 0.921875 5.675781 1.230469 5.414062 1.605469 5.359375 L 6.09375 4.695312 L 8.105469 0.5625 C 8.273438 0.21875 8.621094 0 9.003906 0 C 9.386719 0 9.738281 0.21875 9.902344 0.5625 Z M 11.914062 4.695312 " >< /path> </g>'
+  });
+
+  //timer clock
+  function getTimeRemaining(endtime) {
+    const total = Date.parse(endtime) - Date.parse(new Date());
+    const seconds = Math.floor((total / 1000) % 60);
+    const minutes = Math.floor((total / 1000 / 60) % 60);
+    const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
+    const days = Math.floor(total / (1000 * 60 * 60 * 24));
+
+    return {
+      total,
+      days,
+      hours,
+      minutes,
+      seconds
+    };
+  }
+
+  function initializeClock(id, endtime) {
+    const clock = document.querySelector('.promo__clock');
+    const daysSpan = clock.querySelector('.promo__days');
+    const hoursSpan = clock.querySelector('.promo__hours');
+    const minutesSpan = clock.querySelector('.promo__minutes');
+    const secondsSpan = clock.querySelector('.promo__seconds');
+
+    function updateClock() {
+      const t = getTimeRemaining(endtime);
+
+      daysSpan.innerHTML = t.days;
+      hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+      minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+      secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+      if (t.total <= 0) {
+        clearInterval(timeinterval);
+      }
+    }
+
+    updateClock();
+    const timeinterval = setInterval(updateClock, 1000);
+  }
+
+  const deadline = $('.promo__clock').attr('data-time');
+  initializeClock('promo__clock', deadline);
+
+
+  // mixitup
+  var mixer = mixitup('.article');
+
+});
---
+++ gulpfile.js
@@ -0,0 +1,229 @@
+import gulp from "gulp";
+import dartSass from "sass";
+import gulpSass from "gulp-sass";
+import concat from "gulp-concat";
+import autoprefixer from "gulp-autoprefixer";
+import uglify from "gulp-uglify";
+import bs from "browser-sync";
+import imagemin from "gulp-imagemin";
+import { gifsicle, mozjpeg, optipng, svgo } from "gulp-imagemin";
+import del from "del";
+import fs from "fs";
+import fonter from "gulp-fonter";
+import ttf2woff2 from "gulp-ttf2woff2";
+import fileinclude from "gulp-file-include";
+// import nunjucksRender from "gulp-nunjucks-render"; //1111
+
+const scss = gulpSass(dartSass);
+const browserSync = bs.create();
+// const fileinclude = require('gulp-file-include');
+
+// // Nunjucks Render
+// function nunjucks(){            //11
+//   return src('app/*.njk')       //11
+//     .pipe(nunjucksRender())     //11
+//     .pipe (dest('app'))         //11
+//     .pipe(browserSync.stream()) //11
+// }
+function fileInc() {
+  return gulp
+    .src('app/modul/**.html')
+    .pipe(
+      fileinclude({
+        prefix: '@@',
+        basepath: '@file',
+      })
+    )
+    .pipe(gulp.dest("app/"))
+    .pipe(browserSync.stream());
+}
+
+
+// Server
+
+export const server = () => {
+	browserSync.init({
+		server: {
+			baseDir: "app/",
+		},
+	});
+};
+
+// Remove
+
+export const clearDist = () => {
+	return del("dist");
+};
+
+// Styles
+// css/swiper-bundle.min.css
+export const styles = () => {
+	return gulp
+		.src("app/scss/style.scss")
+		.pipe(scss({ outputStyle: "compressed" }))
+		.pipe(concat("style.min.css"))
+		.pipe(
+			autoprefixer({
+				overrideBrowserslist: ["last 10 versions"],
+				grid: true,
+			})
+		)
+		.pipe(gulp.dest("app/css"))
+		.pipe(browserSync.stream());
+};
+
+// Scripts
+
+export const scripts = () => {
+	return gulp
+		.src([
+      "node_modules/jquery/dist/jquery.js",
+			"node_modules/swiper/swiper-bundle.min.js",
+			"node_modules/mixitup/dist/mixitup.min.js",
+      "node_modules/slick-carousel/slick/slick.js",
+      "node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js",
+      "node_modules/rateyo/src/jquery.rateyo.js",
+      "node_modules/ion-rangeslider/js/ion.rangeSlider.js",
+      "node_modules/jquery-form-styler/dist/jquery.formstyler.js",
+			"app/js/main.js",
+		])
+		.pipe(concat("main.min.js"))
+		.pipe(uglify())
+		.pipe(gulp.dest("app/js"))
+		.pipe(browserSync.stream());
+};
+
+// Images
+
+export const images = () => {
+	return gulp
+		.src("app/images/**/*.*")
+		.pipe(
+			imagemin([
+				gifsicle({ interlaced: true }),
+				mozjpeg({ quality: 75, progressive: true }),
+				optipng({ optimizationLevel: 5 }),
+				svgo({
+					plugins: [{ removeViewBox: true }, { cleanupIDs: false }],
+				}),
+			])
+		)
+		.pipe(gulp.dest("dist/images"));
+};
+
+// Fonts
+
+export const convertFonts = () => {
+	return gulp
+		.src("app/fonts/*ttf")
+		.pipe(
+			fonter({
+				formats: ["woff"],
+			})
+		)
+		.pipe(gulp.dest("app/fonts"))
+		.pipe(gulp.src("app/fonts/*ttf"))
+		.pipe(ttf2woff2())
+		.pipe(gulp.dest("app/fonts"));
+};
+
+export const updateFontsStyle = () => {
+	const fontsStyle = "app/scss/_fonts.scss";
+
+	fs.readdir("app/fonts", (err, fontsFiles) => {
+		if (fontsFiles) {
+			if (!fs.existsSync(fontsStyle)) {
+				fs.writeFile(fontsStyle, "", cb);
+
+				console.log(fontsFiles.length);
+
+				fontsFiles.forEach((fontFile) => {
+					if (fontFile.includes("ttf")) {
+						const fontFileName = fontFile.split(".")[0];
+
+						const fontName = fontFileName.split("-")[0];
+						let fontWeight = fontFileName
+							.split("-")[1]
+							.toLowerCase();
+
+						switch (fontWeight) {
+							case "light":
+								fontWeight = "300";
+								break;
+							case "regular":
+								fontWeight = "400";
+								break;
+							case "medium":
+								fontWeight = "500";
+								break;
+							case "semibold":
+								fontWeight = "600";
+								break;
+							case "bold":
+								fontWeight = "700";
+								break;
+							case "extrabold":
+								fontWeight = "800";
+								break;
+							case "black":
+								fontWeight = "900";
+								break;
+
+							default:
+								fontWeight = "400";
+						}
+
+						fs.appendFile(
+							fontsStyle,
+							`@font-face {
+  font-family: ${fontName};
+  font-display: swap;
+  font-style: normal;
+  font-weight: ${fontWeight};
+  src: url("../fonts/${fontFileName}.woff2") format("woff2"), url("../fonts/${fontFileName}.woff") format("woff");
+}\r\n`,
+							cb
+						);
+					}
+				});
+			} else {
+				console.log("_fonts.scss file already exists!");
+			}
+		} else {
+			console.log("No fonts file availible!");
+		}
+	});
+
+	return gulp.src("app/");
+	function cb() {}
+};
+
+// Build
+
+let copyToDist = () => {
+	return gulp
+		.src(
+			[
+				"app/**/*.html",
+				"app/js/**/*min.js",
+				"app/css/**/*.css",
+				"app/fonts/*.woff",
+				"app/fonts/*.woff2",
+			],
+			{ base: "app" }
+		)
+		.pipe(gulp.dest("dist/"));
+};
+
+export const build = gulp.series(clearDist, images, copyToDist);
+
+// Watcher
+
+export const watch = () => {
+  gulp.watch(["app/scss/**/*.scss"], styles);
+	gulp.watch(["app/js/**/*.js", "!app/js/main.min.js"], scripts);
+  gulp.watch(['app/html/**/*.html']).on('change', fileInc);
+	gulp.watch(["app/**/*html"]).on("change", browserSync.reload);
+};
+
+export default gulp.series(gulp.parallel(fileInc, styles, scripts, server, watch));
